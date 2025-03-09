9c1a32d168d2314198ef99ee50ea3cf08dc4d2d1
sinsujung <jhw02045@naver.com>
2025-03-09 19:52:01 +0900

    InputForm생성하기

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>안녕하세요</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MainScreen />
   );
 }
 
