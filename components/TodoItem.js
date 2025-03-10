4ae329237cd733363af0344e454ad78aaea4dc22
sinsujung <jhw02045@naver.com>
2025-03-11 04:50:14 +0900

    svg 사용하기

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,32 +1,70 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import CheckboxUnchecked from "../assets/checkbox-unchecked.svg";
+import CheckboxChecked from "../assets/checkbox-checked.svg";
+import DeleteIcon from "../assets/delete.svg";
 
 const TodoItem = () => {
   return (
     <View style={styles.itemContainer}>
-        <Pressable
-            hitSlop={10}
-            style={styles.itemTextChecked}
-        >
-
-        </Pressable>
-        <Text style={[styles.itemText, styles.itemTextChecked]}>
-            코딩하기
-        </Text>
-        <Pressable 
-            style={[
-                styles.deleteButton,
-                styles.deleteButtonDone
-            ]}
-            hitSlop={10}
-        >
-
-        </Pressable>
-      <Text>TodoItem</Text>
+      <Pressable hitSlop={10} style={styles.itemCheckbox}>
+        <CheckboxUnchecked />
+        <CheckboxChecked style={styles.itemCheckboxCheckedIcon} />
+      </Pressable>
+      <Text style={[styles.itemText, styles.itemTextChecked]}>코딩하기</Text>
+      <Pressable
+        style={[styles.deleteButton, styles.deleteButtonDone]}
+        hitSlop={10}
+      >
+        <DeleteIcon />
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#f7f8fa",
+  },
+  itemCheckbox: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 2,
+    height: 20,
+    marginRight: 13,
+    borderRadius: 6,
+  },
+  itemCheckboxCheckedIcon: {
+    shadowColor: "#000000",
+    shadowOffset: 0.14,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+  },
+  itemText: {
+    marginRight: "auto",
+    paddingRigth: 25,
+    fontSize: 15,
+    lineHeight: 20,
+    color: "#737373",
+  },
+  itemTextChecked: {
+    opacity: 0.3,
+    textDecorationLine: "line-through",
+  },
+  deleteButton: {
+    opacity: 0.8,
+  },
+  deleteButtonDone: {
+    opacity: 0.3,
+  },
+});
