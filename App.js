c16db8ed3a9a5a9ae604c6348e3711484ac576b8
sinsujung <jhw02045@naver.com>
2025-03-14 13:21:54 +0900

    Todo item 생성하기(addTodo)

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainScreen from './screens/MainScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainScreen from "./screens/MainScreen";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export default function App() {
   return (
-    <MainScreen />
+    <Provider store={store}>
+      <MainScreen />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/InputForm.js
+++ components/InputForm.js
@@ -1,54 +1,75 @@
-import { KeyboardAvoidingView, StyleSheet, Text, Platform, Pressable, TextInput } from 'react-native'
-import React from 'react'
-
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  Platform,
+  Pressable,
+  TextInput,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { addTodo } from "../redux/slices/todoSlice";
 const InputForm = () => {
+  const [currentValue, setCurrentValue] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    if (currentValue !== "") {
+      dispatch(addTodo(currentValue));
+      setCurrentValue("");
+    }
+  };
   return (
     <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.addFormContainer}>
-            <TextInput
-                style={styles.inputField}
-                placeholder="할 일을 작성해주세요."
-            />
-            <Pressable style={styles.addButton}>
-                <Text style={styles.addButtonText}>+</Text>
-            </Pressable>
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.addFormContainer}
+    >
+      <TextInput
+        style={styles.inputField}
+        placeholder="할 일을 작성해주세요."
+        value={currentValue}
+        onChangeText={setCurrentValue}
+        onSubmitEditing={handleSubmit}
+      />
+      <Pressable style={styles.addButton} onPress={handleSubmit}>
+        <Text style={styles.addButtonText}>+</Text>
+      </Pressable>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default InputForm
+export default InputForm;
 
 const styles = StyleSheet.create({
-    addFormContainer: {
-        flexDirection: "row",
-        marginTop: 'auto',
-        marginBottom: 30,
-        paddingHorizontal: 20,
-        backgroundColor: '#f7f8fa',
-    },
-    inputField: {
-        flex: 1 ,
-        height: 42,
-        padding: 5,
-        marginRight: 25,
-        borderRadius: 4,
-        borderColor: 'rgba(0, 0, 0.2)',
-        borderWidth: 1,
-        color: '#000000',
-        fontSize: 15,
-        textAlignVertical: 'center'
-    },
-    addButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 42,
-        height: 42,
-        borderRadius: 4,
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
-    },
-    addButtonText: {
-        color: 'white',
-        fontSize: 25
-    }
-})
+  addFormContainer: {
+    flexDirection: "row",
+    marginTop: "auto",
+    marginBottom: 30,
+    paddingHorizontal: 20,
+    backgroundColor: "#f7f8fa",
+  },
+  inputField: {
+    flex: 1,
+    height: 42,
+    padding: 5,
+    marginRight: 25,
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0.2)",
+    borderWidth: 1,
+    color: "#000000",
+    fontSize: 15,
+    textAlignVertical: "center",
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 42,
+    height: 42,
+    borderRadius: 4,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+  },
+  addButtonText: {
+    color: "white",
+    fontSize: 25,
+  },
+});
