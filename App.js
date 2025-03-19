c94a956b3afb108681eea796c56b3662be412d85
sinsujung <jhw02045@naver.com>
2025-03-19 17:12:20 +0900

    React Navigation

--- App.js
+++ App.js
@@ -2,11 +2,29 @@ import { StyleSheet, Text, View } from "react-native";
 import MainScreen from "./screens/MainScreen";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Main"
+            component={MainScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
